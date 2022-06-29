cc4f014bcf8dc6376056561ac19e78e6d6e28576
Bobby Demerchant <Burlock999@gmail.com>
2022-06-29 08:16:10 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,7 +25,9 @@
 
         
         var alint = smart.patient.api.fetchAll({
-                type: 'AllergyIntolerance'
+                type: 'AllergyIntolerance',
+                query: {
+                }
         });
         
         $.when(pt, obv, alint).fail(onError);
